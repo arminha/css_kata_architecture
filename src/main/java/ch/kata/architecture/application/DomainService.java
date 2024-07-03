54c91d869431ebc81fcf0dcfd770968adc30a802
alwinmark <alwin.mark@css.ch>
2024-07-03 11:26:36 +0200

    Step 0: fix Archunit Tests

--- src/main/java/ch/kata/architecture/application/DomainService.java
+++ src/main/java/ch/kata/architecture/application/DomainService.java
@@ -1,7 +1,5 @@
 package ch.kata.architecture.application;
 
-import ch.kata.architecture.adapter.HandsOffMe;
-
 public class DomainService {
     
     private HandsOffMe handsOffMe = new HandsOffMe();
